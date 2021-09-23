b8d325f9cf01c5a46b84afa710f7e2bf553f0acc
Hasan Erdal <hasaerda@hotmail.com>
2021-09-23 14:59:28 +0300

    Header done and Add Single Carousel doc

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = () => {
+    const carouselSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
     return (
-        <div>
-            Carousel
+        <div className="container mx-auto">
+            <h2> Single Item</h2>
+            <Slider {...carouselSettings}>
+                <div>
+                    <h3>1</h3>
+                </div>
+                <div>
+                    <h3>2</h3>
+                </div>
+                <div>
+                    <h3>3</h3>
+                </div>
+                <div>
+                    <h3>4</h3>
+                </div>
+                <div>
+                    <h3>5</h3>
+                </div>
+                <div>
+                    <h3>6</h3>
+                </div>
+            </Slider>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}','./public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: theme => ({
+        'brand-color': '#5d3ebc'
+      })
+    },
   },
   variants: {
     extend: {},
